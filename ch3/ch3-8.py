0db58342c4633e5a3f7ef2ad4044e8c063dca53b
Phuong Nguyen <vanphuong12a2@gmail.com>
2017-11-23 08:18:44 +0100

    Adds statmodels to do linear regression

--- ch3/ch3-8.py
+++ ch3/ch3-8.py
@@ -4,12 +4,34 @@
 # In[1]:
 
 
-get_ipython().magic(u'matplotlib inline')
+get_ipython().magic('matplotlib inline')
 import pandas as pd
+from statsmodels.formula.api import ols
 
 
 # In[2]:
 
 
 autoDataset = pd.read_csv('../datasets/auto.csv')
+
+
+# In[3]:
+
+
+autoDatasetNoMissing = autoDataset[['mpg', 'horsepower']].dropna()
+
+
+# In[4]:
+
+
+mpgHorsePowerOls = ols(formula='mpg ~ horsepower', data=autoDatasetNoMissing).fit()
+
+
+# In[5]:
+
+
+print(mpgHorsePowerOls.summary())
+
+# Is there a relationship between the predictor and the response?
+# - 
 
